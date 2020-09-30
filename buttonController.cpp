5716c03c3c4ff58e281e33beef2b2d1f28a2e378
shubh125 <shubhamgupta.62000@gmail.com>
2020-09-30 23:41:40 +0530

    code and application added

---
+++ buttonController.cpp
@@ -0,0 +1,23 @@
+#include <simplecpp>
+
+main_program{
+  initCanvas();
+
+  float bFx=150,bFy=100, bLx=400,bLy=100, bWidth=150,bHeight=50;
+  Rectangle buttonF(bFx,bFy,bWidth,bHeight), buttonL(bLx,bLy,bWidth,bHeight);
+
+  Text tF(bFx,bFy,"Forward"), tL(bLx,bLy,"Left Turn");
+  Turtle t;
+
+  repeat(100){
+    int clickPos = getClick();
+    int cx = clickPos/65536;
+    int cy = clickPos % 65536;
+
+    if(bFx-bWidth/2<= cx && cx<= bFx+bWidth/2 && 
+       bFy-bHeight/2 <= cy && cy <= bFy+bHeight/2) t.forward(100);
+
+    if(bLx-bWidth/2<= cx && cx<= bLx+bWidth/2 && 
+       bLy-bHeight/2 <= cy && cy <= bLy+bHeight/2) t.left(10);
+  }
+}
